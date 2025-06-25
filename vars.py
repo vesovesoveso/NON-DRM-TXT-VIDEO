8c48ff719f927cee2486b8bd5cdbce39ba02991d
vesovesoveso <vernasoil@proton.me>
2025-06-25 19:46:18 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -5,8 +5,8 @@ import os
 API_ID    = os.environ.get("API_ID", "")
 API_HASH  = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
-ADMIN     = os.environ.get("ADMIN", "1977145189")  
-ADMINS = [1977145189]  # Replace with your real Telegram ID
+ADMIN     = os.environ.get("ADMIN", "7553314265")  
+ADMINS = [7553314265]  # Replace with your real Telegram ID
 # Replace with your Telegram user ID
 
 #WEBHOOK = True  # Don't change this
